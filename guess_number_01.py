7050b5511189a26c86ef06298969c3a969e429d2
stefanimitova <78100952+stefanimitova@users.noreply.github.com>
2021-12-14 17:32:20 +0200

    Update guess_number_01.py

--- guess_number_01.py
+++ guess_number_01.py
@@ -15,6 +15,23 @@ def guess(x):
 
 guess(20)
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high: #the computer would be mistaken if they are equal
+            guess = random.randint(1, x)
+        else:
+            guess = low #could also be high
+        feedback = input(f'Is {guess} too high (H), too low (L) or correct (C)??')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Haha, I have guessed the number {guess}. Give me a harder one!')
+
+computer_guess(27)
 
 
 
